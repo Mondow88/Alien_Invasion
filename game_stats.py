280de6aaa86509e779efa743f3c77a9b8eff3b0e
Mondow88 <duzhaoxu@hotmail.com>
2025-02-12 21:25:55 +0800

    Alien Invasion @Mondow88

--- game_stats.py
+++ game_stats.py
@@ -3,6 +3,7 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self.reset_stat()
-
+    
+    # define method that resets game status
     def reset_stat(self):
         self.ship_left = self.settings.ship_limit
